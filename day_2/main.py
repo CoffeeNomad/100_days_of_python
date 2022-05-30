bad6a2b990414fa86960c50af0d5a138f6b92090
CoffeeNomad <ryanclardy51@gmail.com>
2022-05-30 10:08:37 -0700

    Data type, mathsss, fstrings (tip calculator)

---
+++ day_2/main.py
@@ -0,0 +1,14 @@
+# DAY 2 tip calcualtor
+
+print('Welcome to the tip calculator.')
+bill = float(input('What was the total bill? '))
+peeps = int(input('How many people to split the bill? '))
+tip = int(input('What percentage top would you like to give? 10, 12, or 15? '))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / peeps
+final_bill = round(bill_per_person, 2)
+
+print(f"Each person should pay: ${final_bill}")
